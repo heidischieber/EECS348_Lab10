4111bf1fa40bb5b34c80107ab98d4ebb15a350a0
Heidi <hschieb@ku.edu>
2025-04-10 09:32:45 -0500

    txt file and reading file

--- calculator.cpp
+++ calculator.cpp
@@ -1,1 +1,7 @@
 #include "calculator.hpp"
+#include <fstream> //allows access to all components of header fstream
+#include <iostream> //allows acces to all components of header iostream
+#include <sstream> //allows acces to all components of header sstream
+#include <string> //allows access to all components of header string
+#include <vector> //allows access to all components of header vector 
+
